841b22fa67c61d03ef947fb52c7dd7d73b42ded0
Eduardo Soares <eduschadesoares@users.noreply.github.com>
2018-06-22 23:32:14 -0300

    Update Print.java

--- Exercícios/First Classes/src/main/Print.java
+++ Exercícios/First Classes/src/main/Print.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main;
 
-/**
- *
- * @author goath
- */
 public class Print {    
     //Início método print
     public void printa(String nome) {
